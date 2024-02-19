5c3c5655536837e34f3ca8547f90471a39d22997
FelipeFlamarini <felipeflamarini@hotmail.com>
2024-02-19 02:51:20 -0300

    aula02/ex02 finalizado

--- aula02/exercicios/ex02.py
+++ aula02/exercicios/ex02.py
@@ -1,7 +1,51 @@
 # Dicionário de definição de palavras
-# Melhore a solução proposta para o problema do dicionário de palavras definido anteriormente. Crie uma API com os seguintes métodos:
+# Melhore a solução proposta para o problema do dicionário de palavras definido anteriormente. 
+# Crie uma API com os seguintes métodos:
 
 # set(word, definition): Adiciona ou modifica a definição de uma palavra.
-# get(word): Retorna a definição de uma palavra. Se a palavra não existir no dicionário, retorna uma mensagem indicando que a palavra não foi encontrada.
-# get_word_order(word): Retorna a ordem em que uma palavra foi adicionada/modificada no dicionário. A ordem deve ser um número inteiro representando a posição da palavra na sequência de adições/modificações.
-# get_all_words(): Retorna uma lista com todas as palavras presentes no dicionário, na ordem em que foram adicionadas/modificadas.
+# get(word): Retorna a definição de uma palavra. Se a palavra não existir no dicionário, 
+# retorna uma mensagem indicando que a palavra não foi encontrada.
+# get_word_order(word): Retorna a ordem em que uma palavra foi adicionada/modificada no dicionário. 
+# A ordem deve ser um número inteiro representando a posição da palavra na sequência de adições/modificações.
+# get_all_words(): Retorna uma lista com todas as palavras presentes no dicionário, na ordem em que 
+# foram adicionadas/modificadas.
+from collections import defaultdict, namedtuple
+import datetime
+
+class dictionary:
+    _wordDefinition = namedtuple("wordDefinition", ("definition", "lastUpdated"))
+    
+    def __init__(self):
+        self._words = defaultdict(lambda : self._wordDefinition(0, datetime.datetime.now()))
+            
+    def set(self, word : str, definition : str):
+        self._words[word.lower()] = self._wordDefinition(definition, datetime.datetime.now())
+    
+    def get(self, word : str):
+        return self._words[word].definition if self._words[word].definition != 0 else "Essa palavra não existe nesse dicionário."
+    
+    def get_word_order(self, word : str):
+        for index, wordSearch in enumerate(self.get_all_words()):
+            if wordSearch == word:
+                return index
+    
+    def get_all_words(self):
+        return [word[0] for word in sorted(self._words.items(), key=lambda e : e[1].lastUpdated) if word[1].definition != 0]
+
+dictionary1 = dictionary()
+dictionary1.set("pindaíba", "falta de dinheiro")
+dictionary1.set("gororoba", "comida duvidosa")
+dictionary1.set("bugiganga", "quinquilharia")
+dictionary1.set("bizu", "ideia")
+
+dictionary1.set("gororoba", "comida duvidosa")
+
+print(dictionary1.get("pindaíba"))
+print(dictionary1.get("gororoba"))
+print(dictionary1.get("abc"))
+
+
+print(dictionary1.get_word_order("pindaíba"))
+print(dictionary1.get_word_order("bizu"))
+
+print(dictionary1.get_all_words())
