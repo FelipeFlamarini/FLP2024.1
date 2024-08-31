9cc8be8ed8436785525ce4c8ffbee90449af69ff
Felipe Flamarini <felipeflamarini@hotmail.com>
2024-08-31 00:56:51 +0000

    fix UVa12770

--- exercicios/UVa12770/exercicio UVa12770.py
+++ exercicios/UVa12770/exercicio UVa12770.py
@@ -4,17 +4,20 @@ word = stdin.readline().strip()
 while word != "#":
     str_out = ""
         
-    if len(word) >= 3:
-        char_count = set(word)
-        uneven_chars = set()
-        for char in char_count:
-            if word.count(char) % 2 != 0:
-                uneven_chars.add(char)
-                
-        while len(uneven_chars) > 1:
-            character_min = min(uneven_chars)
-            str_out += character_min
-            uneven_chars.remove(character_min)
+    char_count = {}
+    
+    for char in word:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+        
+    uneven_chars = [char for char, count in char_count.items() if count % 2 != 0]
+            
+    while len(uneven_chars) > 1:
+        character_min = min(uneven_chars)
+        str_out += character_min
+        uneven_chars.remove(character_min)
 
     stdout.write(f"{str_out}\n")
     word = stdin.readline().strip()
